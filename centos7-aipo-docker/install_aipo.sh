a74330fcf090a2f76fb9459fb14f602ee45e21d7
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2017-02-22 12:06:00 +0900

    docker

--- centos7-aipo-docker/install_aipo.sh
+++ centos7-aipo-docker/install_aipo.sh
@@ -36,7 +36,7 @@ docker run hello-world
 # install Docker Compose
 curl -L https://github.com/docker/compose/releases/download/1.11.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+/usr/local/bin/docker-compose --version
 # setup Docker Compose
 cd /docker_data
-docker-compose up -d --build
+/usr/local/bin/docker-compose up -d --build
